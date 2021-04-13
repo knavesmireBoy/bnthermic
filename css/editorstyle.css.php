4a094e8621a8a22fbd7cc5392f95dcffec98d4c3
Andrew Sykes <andrewsykes@btinternet.com>
2021-04-13 10:34:13 +0100

    init

---
+++ css/editorstyle.css.php
@@ -0,0 +1,16 @@
+<?php
+
+@readfile("base.css");
+@readfile("size.css");
+
+?>
+body {
+	margin-left: 10px;
+}
+
+table {
+	font-size: 10px;
+}
+
+ul span { color: #c00; }
+ul li     { background-image: url(../images/db.gif); background-repeat: no-repeat; background-position: 0 0.25em; margin-bottom: 0.5em; padding-left: 1.5em; }
